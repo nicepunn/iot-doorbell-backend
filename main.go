3601e227e80fc54894bba52e8473e87d2933b526
nicepunn <punnawat58000@gmail.com>
2024-09-29 19:28:24 +0700

    feat: firebase

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"context"
 
 	"github.com/gorilla/websocket"
+
+	firebase "firebase.google.com/go/v4"
+	"google.golang.org/api/option"
 )
 
 var upgrader = websocket.Upgrader{
@@ -12,6 +19,7 @@ var upgrader = websocket.Upgrader{
 		return true
 	},
 }
+
 
 var senderConn *websocket.Conn
 var receiverConn *websocket.Conn
@@ -27,28 +35,40 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	clientType := r.URL.Query().Get("type")
 	if clientType == "sender" {
 		senderConn = conn
-		log.Println("Sender connected")
 	} else if clientType == "receiver" {
 		receiverConn = conn
-		log.Println("Receiver connected")
 	}
+	log.Printf("%s connected\n", clientType)
+
 
 	for {
+		log.Printf("%s waiting for message\n", clientType)
 		messageType, msg, err := conn.ReadMessage()
+		log.Printf("%s recieved message\n", clientType)
 		if err != nil {
 			log.Println("Read error:", err)
 			break
 		}
 
 		if clientType == "sender" {
+			fmt.Printf("sender %d", messageType)
 			if receiverConn != nil {
 				err = receiverConn.WriteMessage(messageType, msg)
 				if err != nil {
 					log.Println("Error sending image to receiver:", err)
 					break
+				} else {
+					if messageType == websocket.BinaryMessage {
+						fileName := time.Now().Format("2006-01-02_15:04:05")
+						if err := uploadImageToFirebase(msg, fileName); err != nil {
+							log.Println("Error uploading image:", err)
+							break
+						}
+					}
 				}
 			}
 		} else if clientType == "receiver" {
+			fmt.Printf("reciever %d", messageType)
 			if senderConn != nil {
 				err = senderConn.WriteMessage(messageType, msg)
 				if err != nil {
@@ -60,7 +80,41 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func uploadImageToFirebase(imageData []byte, fileName string) error {
+	ctx := context.Background()
+
+	opt := option.WithCredentialsFile("serviceAccountKey.json")
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		return fmt.Errorf("error initializing app: %v", err)
+	}
+
+	client, err := app.Storage(ctx)
+	if err != nil {
+		return fmt.Errorf("error getting Storage client: %v", err)
+	}
+
+	bucketName := "smart-doorbell-9bd31.appspot.com"
+	bucket, err := client.Bucket(bucketName)
+	if err != nil {
+		return fmt.Errorf("error getting bucket: %v", err)
+	}
+
+	writer := bucket.Object(fileName).NewWriter(ctx)
+	if _, err = writer.Write(imageData); err != nil {
+		writer.Close()
+		return fmt.Errorf("error writing to bucket: %v", err)
+	}
+	if err = writer.Close(); err != nil {
+		return fmt.Errorf("error closing writer: %v", err)
+	}
+
+	fmt.Printf("Image uploaded to Firebase Storage: %s\n", fileName)
+	return nil
+}
+
 func main() {
+
 	http.HandleFunc("/ws", wsHandler)
 	http.Handle("/", http.FileServer(http.Dir("./test-frontend"))) // Just for testing with mock-up frontend
 	log.Fatal(http.ListenAndServe(":8080", nil))
